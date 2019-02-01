04ab3ad6fcdf687ae8511d4875e5073a01559fd7
Adam Kirschner <>
2019-01-31 21:48:16 -0600

    some quick examples on inputs in react vs. html rendered by PHP

---
+++ react_security/security_examples/components/EscapeInput.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+// example to escape strings by default
+
+const inputString = "<input type='text' defaultValue='rendered' />"
+
+class EscapeInput extends Component {
+
+    constructor() {
+        super()
+        this.state = { title: '' }
+    }
+
+    updateTitle = event => this.setState({ title: event.target.value })
+
+    render() {
+        return (
+            <div>
+                <h3>Safe React Input</h3>
+                <div>{inputString}</div>
+                <h4>{this.state.title}</h4>
+                <br/>
+                <input placeholder ='set title...' onChange={this.updateTitle}/>
+                <br/>
+            </div>
+        )
+    }
+}
+
+export default EscapeInput

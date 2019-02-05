66f88dc222dd8106a0a7f5618e3d5cf1381d6a39
Adam Kirschner <>
2019-02-04 21:23:28 -0600

    quick refactor

--- server/app.js
+++ server/app.js
@@ -1,58 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const SHA256 = require('crypto-js/sha256')
-const pool = require('./db')
-const { APP_SECRET } = require('./secrets')
+const user = require('./user')
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use('/user', user)
 
-const hash = str => { 
-    return SHA256(`${APP_SECRET}${str}${APP_SECRET}`).toString()
-}
-
-app.get('/users/all', (req, res, next) => {
-    pool.query('SELECT * FROM users', (q_err, q_res) => {
-        if (q_err) return next(q_err)
-
-        res.json(q_res.rows)
-    })
-})
-
-app.post('/user/new', (req, res, next) => {
-    const { username, password } = req.body
-
-    const username_hash = hash(username)
-
-    pool.query(
-        'SELECT * FROM users WHERE username_hash = $1',
-        [username_hash],
-        (q0_err, q0_res) => {
-            if (q0_err) return next(q0_err)
-            if (q0_res.rows.length === 0) {
-                pool.query(
-                    'INSERT INTO users(username_hash, password_hash) VALUES($1, $2)',
-                    [username_hash, hash(password)],
-                    (q1_err, q_1res) => {
-                        if (q1_err) return next(q1_err)
-                        res.json({ msg: 'Successfully created user!'})
-                    }
-                )
-            }
-            else {
-                res.status(409).json({ 
-                    type: 'error',
-                    msg: 'This username is taken'
-                })
-            }
-        }
-    )
-})
 
 app.use((err, req, res, next) => {
     if (!err.statusCode) err.statusCode = 500
-
     res.status(err.statusCode).json({ 
         type: 'error', 
         msg: err.message 
---
+++ server/user.js
@@ -0,0 +1,50 @@
+const { Router } = require('express')
+const SHA256 = require('crypto-js/sha256')
+const pool = require('./db')
+const { APP_SECRET } = require('./secrets')
+
+const router = new Router()
+
+const hash = str => {
+    return SHA256(`${APP_SECRET}${str}${APP_SECRET}`).toString()
+}
+
+router.get('/all', (req, res, next) => {
+    pool.query('SELECT * FROM users', (q_err, q_res) => {
+        if (q_err) return next(q_err)
+
+        res.json(q_res.rows)
+    })
+})
+
+router.post('/new', (req, res, next) => {
+    const { username, password } = req.body
+
+    const username_hash = hash(username)
+
+    pool.query(
+        'SELECT * FROM users WHERE username_hash = $1',
+        [username_hash],
+        (q0_err, q0_res) => {
+            if (q0_err) return next(q0_err)
+            if (q0_res.rows.length === 0) {
+                pool.query(
+                    'INSERT INTO users(username_hash, password_hash) VALUES($1, $2)',
+                    [username_hash, hash(password)],
+                    (q1_err, q_1res) => {
+                        if (q1_err) return next(q1_err)
+                        res.json({ msg: 'Successfully created user!' })
+                    }
+                )
+            }
+            else {
+                res.status(409).json({
+                    type: 'error',
+                    msg: 'This username is taken'
+                })
+            }
+        }
+    )
+})
+
+module.exports = router
